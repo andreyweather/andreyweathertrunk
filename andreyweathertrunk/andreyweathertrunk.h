74a1091a455ba28bf072b88c38c82d770317fb1f
andreyPogodin <andreyweather@gmail.com>
2024-04-19 12:32:15 +0300

    commit

---
+++ andreyweathertrunk/andreyweathertrunk.h
@@ -0,0 +1,18 @@
+//
+//  andreyweathertrunk.h
+//  andreyweathertrunk
+//
+//  Created by andrey.pogodin on 19.04.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for andreyweathertrunk.
+FOUNDATION_EXPORT double andreyweathertrunkVersionNumber;
+
+//! Project version string for andreyweathertrunk.
+FOUNDATION_EXPORT const unsigned char andreyweathertrunkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <andreyweathertrunk/PublicHeader.h>
+
+
---
+++ andreyweathertrunk/andreyweathertrunk.swift
@@ -0,0 +1,12 @@
+//
+//  andreyweathertrunk.swift
+//  andreyweathertrunk
+//
+//  Created by andrey.pogodin on 19.04.2024.
+//
+
+import Foundation
+
+public func test () {
+    print("hi! This is my first public pod!")
+}
